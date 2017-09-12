737b8b7d33b16a0fa6007dba1091b2efb0690270
Maciek-WINMAC <maciek.anuszkiewicz@gmail.com>
2017-09-12 06:05:45 +0200

    cathZeroGame

--- udeTom1.0/Form1.Designer.cs
+++ udeTom1.0/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace udeTom1._0
 {
-    partial class Form1
+    partial class buf
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1z = new System.Windows.Forms.Button();
+            this.tmp = new System.Windows.Forms.Label();
+            this.button2z = new System.Windows.Forms.Button();
+            this.button3z = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sum1 = new System.Windows.Forms.Label();
+            this.sum2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button8z = new System.Windows.Forms.Button();
+            this.button7z = new System.Windows.Forms.Button();
+            this.button6z = new System.Windows.Forms.Button();
+            this.button9z = new System.Windows.Forms.Button();
+            this.button4z = new System.Windows.Forms.Button();
+            this.button10z = new System.Windows.Forms.Button();
+            this.button5z = new System.Windows.Forms.Button();
+            this.diff = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1z
+            // 
+            this.button1z.Location = new System.Drawing.Point(73, 69);
+            this.button1z.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1z.Name = "button1z";
+            this.button1z.Size = new System.Drawing.Size(53, 50);
+            this.button1z.TabIndex = 0;
+            this.button1z.UseVisualStyleBackColor = true;
+            this.button1z.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tmp
+            // 
+            this.tmp.AutoSize = true;
+            this.tmp.Location = new System.Drawing.Point(609, 25);
+            this.tmp.Name = "tmp";
+            this.tmp.Size = new System.Drawing.Size(29, 31);
+            this.tmp.TabIndex = 1;
+            this.tmp.Text = "0";
+            // 
+            // button2z
+            // 
+            this.button2z.Location = new System.Drawing.Point(130, 69);
+            this.button2z.Margin = new System.Windows.Forms.Padding(2);
+            this.button2z.Name = "button2z";
+            this.button2z.Size = new System.Drawing.Size(53, 50);
+            this.button2z.TabIndex = 2;
+            this.button2z.Text = "1";
+            this.button2z.UseVisualStyleBackColor = true;
+            this.button2z.Click += new System.EventHandler(this.button2z_Click);
+            // 
+            // button3z
+            // 
+            this.button3z.Location = new System.Drawing.Point(187, 69);
+            this.button3z.Margin = new System.Windows.Forms.Padding(2);
+            this.button3z.Name = "button3z";
+            this.button3z.Size = new System.Drawing.Size(53, 50);
+            this.button3z.TabIndex = 3;
+            this.button3z.Text = "2";
+            this.button3z.UseVisualStyleBackColor = true;
+            this.button3z.Click += new System.EventHandler(this.button3z_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "x";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(400, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "=";
+            // 
+            // sum1
+            // 
+            this.sum1.AutoSize = true;
+            this.sum1.Location = new System.Drawing.Point(436, 79);
+            this.sum1.Name = "sum1";
+            this.sum1.Size = new System.Drawing.Size(80, 31);
+            this.sum1.TabIndex = 7;
+            this.sum1.Text = "sum1";
+            // 
+            // sum2
+            // 
+            this.sum2.AutoSize = true;
+            this.sum2.Location = new System.Drawing.Point(436, 145);
+            this.sum2.Name = "sum2";
+            this.sum2.Size = new System.Drawing.Size(86, 31);
+            this.sum2.TabIndex = 14;
+            this.sum2.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(400, 145);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 31);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "=";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(245, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 31);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "x";
+            // 
+            // button8z
+            // 
+            this.button8z.Location = new System.Drawing.Point(187, 135);
+            this.button8z.Margin = new System.Windows.Forms.Padding(2);
+            this.button8z.Name = "button8z";
+            this.button8z.Size = new System.Drawing.Size(53, 50);
+            this.button8z.TabIndex = 10;
+            this.button8z.Text = "7";
+            this.button8z.UseVisualStyleBackColor = true;
+            this.button8z.Click += new System.EventHandler(this.button8z_Click);
+            // 
+            // button7z
+            // 
+            this.button7z.Location = new System.Drawing.Point(130, 135);
+            this.button7z.Margin = new System.Windows.Forms.Padding(2);
+            this.button7z.Name = "button7z";
+            this.button7z.Size = new System.Drawing.Size(53, 50);
+            this.button7z.TabIndex = 9;
+            this.button7z.Text = "6";
+            this.button7z.UseVisualStyleBackColor = true;
+            this.button7z.Click += new System.EventHandler(this.button7z_Click);
+            // 
+            // button6z
+            // 
+            this.button6z.Location = new System.Drawing.Point(73, 135);
+            this.button6z.Margin = new System.Windows.Forms.Padding(2);
+            this.button6z.Name = "button6z";
+            this.button6z.Size = new System.Drawing.Size(53, 50);
+            this.button6z.TabIndex = 8;
+            this.button6z.Text = "5";
+            this.button6z.UseVisualStyleBackColor = true;
+            this.button6z.Click += new System.EventHandler(this.button6z_Click);
+            // 
+            // button9z
+            // 
+            this.button9z.Location = new System.Drawing.Point(277, 135);
+            this.button9z.Margin = new System.Windows.Forms.Padding(2);
+            this.button9z.Name = "button9z";
+            this.button9z.Size = new System.Drawing.Size(53, 50);
+            this.button9z.TabIndex = 16;
+            this.button9z.Text = "8";
+            this.button9z.UseVisualStyleBackColor = true;
+            this.button9z.Click += new System.EventHandler(this.button9z_Click);
+            // 
+            // button4z
+            // 
+            this.button4z.Location = new System.Drawing.Point(277, 69);
+            this.button4z.Margin = new System.Windows.Forms.Padding(2);
+            this.button4z.Name = "button4z";
+            this.button4z.Size = new System.Drawing.Size(53, 50);
+            this.button4z.TabIndex = 15;
+            this.button4z.Text = "3";
+            this.button4z.UseVisualStyleBackColor = true;
+            this.button4z.Click += new System.EventHandler(this.button4z_Click);
+            // 
+            // button10z
+            // 
+            this.button10z.Location = new System.Drawing.Point(334, 135);
+            this.button10z.Margin = new System.Windows.Forms.Padding(2);
+            this.button10z.Name = "button10z";
+            this.button10z.Size = new System.Drawing.Size(53, 50);
+            this.button10z.TabIndex = 18;
+            this.button10z.Text = "9";
+            this.button10z.UseVisualStyleBackColor = true;
+            this.button10z.Click += new System.EventHandler(this.button10z_Click);
+            // 
+            // button5z
+            // 
+            this.button5z.Location = new System.Drawing.Point(334, 69);
+            this.button5z.Margin = new System.Windows.Forms.Padding(2);
+            this.button5z.Name = "button5z";
+            this.button5z.Size = new System.Drawing.Size(53, 50);
+            this.button5z.TabIndex = 17;
+            this.button5z.Text = "4";
+            this.button5z.UseVisualStyleBackColor = true;
+            this.button5z.Click += new System.EventHandler(this.button5z_Click);
+            // 
+            // diff
+            // 
+            this.diff.AutoSize = true;
+            this.diff.Location = new System.Drawing.Point(436, 217);
+            this.diff.Name = "diff";
+            this.diff.Size = new System.Drawing.Size(86, 31);
+            this.diff.TabIndex = 19;
+            this.diff.Text = "label2";
+            // 
+            // buf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 462);
+            this.Controls.Add(this.diff);
+            this.Controls.Add(this.button10z);
+            this.Controls.Add(this.button5z);
+            this.Controls.Add(this.button9z);
+            this.Controls.Add(this.button4z);
+            this.Controls.Add(this.sum2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button8z);
+            this.Controls.Add(this.button7z);
+            this.Controls.Add(this.button6z);
+            this.Controls.Add(this.sum1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3z);
+            this.Controls.Add(this.button2z);
+            this.Controls.Add(this.tmp);
+            this.Controls.Add(this.button1z);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "buf";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1z;
+        private System.Windows.Forms.Label tmp;
+        private System.Windows.Forms.Button button2z;
+        private System.Windows.Forms.Button button3z;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label sum1;
+        private System.Windows.Forms.Label sum2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button8z;
+        private System.Windows.Forms.Button button7z;
+        private System.Windows.Forms.Button button6z;
+        private System.Windows.Forms.Button button9z;
+        private System.Windows.Forms.Button button4z;
+        private System.Windows.Forms.Button button10z;
+        private System.Windows.Forms.Button button5z;
+        private System.Windows.Forms.Label diff;
     }
 }
 
--- udeTom1.0/Form1.cs
+++ udeTom1.0/Form1.cs
@@ -1,20 +1,115 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace udeTom1._0
 {
-    public partial class Form1 : Form
+    public partial class buf : Form
     {
-        public Form1()
+        public buf()
         {
             InitializeComponent();
+        }
+        void calculate()
+        {
+            string n1 = button1z.Text + button2z.Text + button3z.Text;
+            string n2 = button4z.Text + button5z.Text;
+            string n3 = button6z.Text + button7z.Text + button8z.Text;
+            string n4 = button9z.Text + button10z.Text;
+
+            int l1 = int.Parse(n1);
+            int l2 = int.Parse(n2);
+            int l3 = int.Parse(n3);
+            int l4 = int.Parse(n4);
+
+            int suma1 = l1 * l2;
+            sum1.Text = $"{suma1}";
+            int suma2 = l3 * l4;
+            sum2.Text = $"{suma2}";
+            int diffVal = suma1 - suma2;
+            diff.Text = $"{diffVal}";
+        }
+        bool load;
+        string inTmp;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inTmp = button1z.Text;
+            button1z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+           
+        }
+
+        private void button2z_Click(object sender, EventArgs e)
+        {
+            inTmp = button2z.Text;
+            button2z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+
+        }
+
+        private void button3z_Click(object sender, EventArgs e)
+        {
+            inTmp = button3z.Text;
+            button3z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+        }
+
+        private void button4z_Click(object sender, EventArgs e)
+        {
+            inTmp = button4z.Text;
+            button4z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+        }
+
+        private void button5z_Click(object sender, EventArgs e)
+        {
+            inTmp = button5z.Text;
+            button5z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+        }
+
+        private void button6z_Click(object sender, EventArgs e)
+        {
+            inTmp = button6z.Text;
+            button6z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+        }
+
+        private void button7z_Click(object sender, EventArgs e)
+        {
+            inTmp = button7z.Text;
+            button7z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+        }
+
+        private void button8z_Click(object sender, EventArgs e)
+        {
+            inTmp = button8z.Text;
+            button8z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+        }
+
+        private void button9z_Click(object sender, EventArgs e)
+        {
+            inTmp = button9z.Text;
+            button9z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
+        }
+
+        private void button10z_Click(object sender, EventArgs e)
+        {
+            inTmp = button10z.Text;
+            button10z.Text = tmp.Text;
+            tmp.Text = inTmp;
+            calculate();
         }
     }
 }
